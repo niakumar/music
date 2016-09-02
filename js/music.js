264dde4e1d8aba7c588c0c4d3270d4228b9a8d37
satishkumar0389 <satishkumar0389@gmail.com>
2016-09-02 16:19:38 +0530

    modified song name for play

--- js/music.js
+++ js/music.js
@@ -15,6 +15,8 @@
     window.onload = function(){
       AudioJS.setup();
       $('.playlist').insertBefore(".vjs-volume-control");
+      $('.play_sg_name').insertBefore('.vjs-progress-holder')
+
       $('.upload_songs').click(function(){
       	$('.overlay, .upload_popup').addClass('display_block');
       	$('body').addClass('overflow_hidden');
